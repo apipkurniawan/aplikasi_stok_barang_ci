bd8a1d44bed4556999730b30b8e0f726fff20aea
Apip Kurniawan <davitaja92@gmail.com>
2023-12-06 10:33:30 +0700

    fix pdf penerimaan

--- application/views/penerimaan/report.php
+++ application/views/penerimaan/report.php
@@ -8,34 +8,38 @@
 </head>
 
 <body>
-    <div class="row">
+    <div class="row" style="margin-bottom: 40px;">
         <div class="col text-center">
-            <h3 class="h3 text-dark"><?= $title ?></h3>
+            <h2 class="h2 text-dark"><?= $title ?></h2>
             <!-- <h4 class="h4 text-dark "><strong><?= $perusahaan->nama_perusahaan ?></strong></h4> -->
+            <hr>
         </div>
     </div>
-    <hr>
     <div class="row">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
                 <tr>
-                    <td>No Terima</td>
-                    <td>Kode Barang</td>
-                    <td>Jumlah</td>
-                    <td>Kode Petugas</td>
-                    <td>Kode Supplier</td>
-                    <td>Tanggal Terima</td>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">No</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">No. Terima</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">Kode Barang</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">Jumlah</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">Kode Petugas</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">Kode Supplier</th>
+                    <th style="font-weight: 600; border: 1px solid black; padding: 8px;">Tanggal Terima</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($all_penerimaan as $penerimaan): ?>
                 <tr>
-                    <td><?= $penerimaan->no_terima ?></td>
-                    <td><?= $penerimaan->kode_barang ?></td>
-                    <td><?= $penerimaan->jumlah ?></td>
-                    <td><?= $penerimaan->kode_petugas ?></td>
-                    <td><?= $penerimaan->kode_supplier ?></td>
-                    <td><?= $penerimaan->tgl_terima ?> Pukul <?= $penerimaan->jam_terima ?></td>
+                    <td style="border: 1px solid black; padding: 8px; text-align:center"><?= $no++ ?></td>
+                    <td style="border: 1px solid black; padding: 8px;"><?= $penerimaan->no_terima ?>
+                    </td>
+                    <td style="border: 1px solid black; padding: 8px;"><?= $penerimaan->kode_barang ?></td>
+                    <td style="border: 1px solid black; padding: 8px; text-align:center"><?= $penerimaan->jumlah ?></td>
+                    <td style="border: 1px solid black; padding: 8px;"><?= $penerimaan->kode_petugas ?></td>
+                    <td style="border: 1px solid black; padding: 8px;"><?= $penerimaan->kode_supplier ?></td>
+                    <td style="border: 1px solid black; padding: 8px;"><?= $penerimaan->tgl_terima ?>
+                        <?= $penerimaan->jam_terima ?></td>
                 </tr>
                 <?php endforeach ?>
             </tbody>
